e68950a7bc2b8a4b2d59c84f755ee7e8c8fede85
Samoila Gabor <samoila.gabor@iquestgroup.com>
2021-01-06 12:26:50 +0200

    randomly update the price every second

--- src/components/Coin/Coin.jsx
+++ src/components/Coin/Coin.jsx
@@ -1,14 +1,30 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types';
 
-
 export default class Coin extends Component {
+  constructor(props) {
+    super(props);
+  
+    this.state = {
+      price: this.props.price
+    }
+  }
+
+  componentDidMount() {
+    setInterval(() => this.refreshPrice(), 1000);
+  }
+
+  refreshPrice = () => {
+    const randomPercentage = 0.995 + Math.random() * 0.01;
+    this.setState((state) => ({ price: Math.round(state.price * randomPercentage * 1000) / 1000 }));
+  }
+
   render() {
     return (
       <tr>
         <td>{ this.props.name }</td>
         <td>{ this.props.ticker }</td>
-        <td>${ this.props.price }</td>
+        <td>${ this.state.price }</td>
       </tr>
     )
   }
