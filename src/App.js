674f147ffd3ecd1dbfc932d0b1893a48f139bc6e
Samoila Gabor <samoila.gabor@iquestgroup.com>
2021-01-06 11:09:13 +0200

    initial exchange structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import Coin from './components/Coin/Coin';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Coin Exchange</h1>
       </header>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Ticker</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          <Coin name="Bitcoin" ticker="BTC" price={35000}/>
+          <Coin name="Ether" ticker="ETH" price={1100}/>
+          <Coin name="Tether" ticker="USDT" price={1}/>
+        </tbody>
+      </table>
     </div>
   );
 }
