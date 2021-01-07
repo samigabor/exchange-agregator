7d12f7f66d258a6ac9c74de5567d888cce86806a
Samoila Gabor <samoila.gabor@iquestgroup.com>
2021-01-07 10:41:41 +0200

    centralize app state

--- src/App.js
+++ src/App.js
@@ -1,32 +1,65 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Coin from './components/Coin/Coin';
 import AccountBalance from './components/AccountBalance/AccountBalance';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Coin Exchange</h1>
-      </header>
-      <AccountBalance amount={10000}/>
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Ticker</th>
-            <th>Price</th>
-          </tr>
-        </thead>
-        <tbody>
-          <Coin name="Bitcoin" ticker="BTC" price={35000}/>
-          <Coin name="Ether" ticker="ETH" price={1100}/>
-          <Coin name="Tether" ticker="USDT" price={1}/>
-        </tbody>
-      </table>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+      balance: 10000,
+      coins: [
+        {
+          name: 'Bitcoin',
+          ticker: 'BTC',
+          price: 37000
+        },
+        {
+          name: 'Bitcoin Cash',
+          ticker: 'BCH',
+          price: 400
+        },
+        {
+          name: 'Ether',
+          ticker: 'ETH',
+          price: 1200
+        },
+        {
+          name: 'Theter',
+          ticker: 'USDT',
+          price: 1
+        }
+      ]
+    }
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Coin Exchange</h1>
+        </header>
+        <AccountBalance amount={this.state.balance}/>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Ticker</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              this.state.coins.map(({name, ticker, price}) => <Coin name={name} ticker={ticker} price={price} key={ticker}/>)
+            }
+          </tbody>
+        </table>
+      </div>
+    )
+  }
 }
 
 export default App;
